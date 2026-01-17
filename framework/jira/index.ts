c8192ea5d30f44a3ad26209de3238961a9479149
Andrew Novykov <papapin777@gmail.com>
2026-01-17 00:10:59 -0800

    Remove code (#46)
    * remove code
    
    * Remove .env file with secrets
    
    * Add .env to gitignore
    
    * Fix .gitignore to properly exclude .env files

--- framework/jira/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * JIRA Integration for Endorphin AI
- * Main export file for JIRA-related functionality
- */
-
-export { JiraClient } from './jira-client.js';
-export { JiraConverter } from './jira-converter.js';
-export { JiraStorage } from './jira-storage.js';
-export type { ConversionResult } from './jira-converter.js';
